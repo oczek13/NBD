e9f84063dc0af6d204efd58464720175612ae3c2
oczek13 <236616@edu.p.lodz.pl>
2022-10-18 23:31:55 +0200

    Fajrant

--- src/main/java/com/base/front/MainApp.java
+++ src/main/java/com/base/front/MainApp.java
@@ -1,7 +1,5 @@
 package com.base.front;
 
-import com.base.managers.ClientManager;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class MainApp {
     public static void main(String[] args) {
